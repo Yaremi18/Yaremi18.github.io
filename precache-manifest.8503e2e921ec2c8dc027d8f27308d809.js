63c1ecf42aa5efd2977d55e70f7d64a600328a89
Yaremi Astorga <yare1894@gmail.com>
2019-05-28 13:23:13 -0500

    Start my personal web page

---
+++ precache-manifest.8503e2e921ec2c8dc027d8f27308d809.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "020c58d18282a54595deb38b91f5fe04",
+    "url": "/index.html"
+  },
+  {
+    "revision": "1d77d195e8aeb1fa39d7",
+    "url": "/static/css/main.c6531d5b.chunk.css"
+  },
+  {
+    "revision": "2315f74ce3c99316133d",
+    "url": "/static/js/2.3310f33a.chunk.js"
+  },
+  {
+    "revision": "1d77d195e8aeb1fa39d7",
+    "url": "/static/js/main.3236592b.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
